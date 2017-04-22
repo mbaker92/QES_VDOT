f2ad71ea6dfc426a528e41eef14d474475e85edb
Matthew Baker <hackmattr@gmail.com>
2017-04-21 20:08:53 -0700

    Init Commit

---
+++ NewVdotProcessing/NewVdotProcessing/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace NewVdotProcessing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.Browser = new System.Windows.Forms.Button();
+            this.Start = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(42, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(399, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "NewDatabase";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter Name of New Database";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Select Folder Location";
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.Description = "Choose Folder Location of New Database";
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // Browser
+            // 
+            this.Browser.Location = new System.Drawing.Point(366, 94);
+            this.Browser.Name = "Browser";
+            this.Browser.Size = new System.Drawing.Size(75, 23);
+            this.Browser.TabIndex = 3;
+            this.Browser.Text = "Browse";
+            this.Browser.UseVisualStyleBackColor = true;
+            this.Browser.Click += new System.EventHandler(this.Browser_Click);
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(212, 153);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(75, 23);
+            this.Start.TabIndex = 4;
+            this.Start.Text = "START";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Folder Location";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(497, 188);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.Browser);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "VDOT Processing";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button Browser;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ NewVdotProcessing/NewVdotProcessing/Form1.cs
@@ -0,0 +1,259 @@
+﻿/*
+ * Author: Matthew Baker
+ * Program: NewVdotProcessing
+ * Date: April 1, 2017
+ * 
+ * Details: C# program to run Access and Excel Scripts on Databases for VDOT.
+ * 
+ * 
+ * TODO : - Uncomment ShoulderMacro Stuff when Shoulder Scripts are ready
+ *        - Test Program on Databases with Shoulder Scripts
+*/
+using System;
+using System.Reflection;
+using Microsoft.Office.Core;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Access = Microsoft.Office.Interop.Access;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace NewVdotProcessing
+{
+    public partial class Form1 : Form
+    {
+        private string pathfull;
+        public string path { get { return pathfull; } set { pathfull = value; } }
+        bool choseFolder = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        /* Browser Click
+         *  Will show a folder browse window. If folder was choosen, but Start was not clicked. Delete the Macros
+         *  Store the Path
+         *  Show path in the window
+         *  Rename the old processed excel files if they exist in the folder
+         *  Set the path of the excel and access scripts
+         *  Combine the path and the user's name of new database
+         *  Rename Database in folder if already exists
+         *  Copy new database and excel macros to the chosen folder
+         *  Set chosenFolder to true 
+         */
+
+        private void Browser_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if(choseFolder == true)
+                {
+                    deleteFile("QAResultsACPMac.xlsm");
+                    deleteFile("CRCMac.xlsm");
+                    deleteFile("JCPMac.xlsm");
+                    //  deleteFile("ShoulderComparisonMac.xlsm");
+                }
+
+
+                // Get the Path of Where the user wants the new Database
+                path = folderBrowserDialog1.SelectedPath;
+
+
+                // Set Label3 to display the path the the user chose
+                label3.Text = path;
+
+
+                // If there are old files from previous processing, Rename them with time in appended infront of name
+                RenameOldFiles();
+
+
+                // The File Path with the Excel Scripts
+                string ExcelPath = " "; // Removed Actual Directory
+                string AccessPath = " "; // Removed Actual Directory
+                
+                
+                // Combine the path selected by user with the name of the database without extension
+                string DestFile = System.IO.Path.Combine(path, textBox1.Text);
+  
+                // Add Extension
+                DestFile += ".accdb";
+
+
+                //Check if File Exists. If it does append time to front of old file
+                if (System.IO.File.Exists(DestFile))
+                {
+                    System.IO.File.Move(DestFile, path + @"\" + DateTime.Now.ToString("HHmmss") + textBox1.Text + @".accdb");
+                }
+
+
+                // Copy Access Database with scripts over to Location specifed by user with the extension and changed name.
+                System.IO.File.Copy(AccessPath, DestFile , false);
+                
+                
+                // Copy Excel Files over
+                System.IO.File.Copy(ExcelPath + "QAResultsACPMac.xlsm", path + @"\QAResultsACPMac.xlsm", true);
+                System.IO.File.Copy(ExcelPath + "CRCMac.xlsm", path + @"\CRCMac.xlsm", true);
+                System.IO.File.Copy(ExcelPath + "JCPMac.xlsm", path + @"\JCPMac.xlsm", true);
+                // System.IO.File.Copy(ExcelPath + "ShoulderComparisonMac.xlsm", path + @"\ShoulderComparisonMac.xlsm", true);
+
+
+                choseFolder = true;
+
+            }
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        /* Start_Click Will start and run Access Macro.
+         * Once Access is done, the function will run the excel macros,
+         * delete the excel macro files from the directory, and let the user know its done.
+         */
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            if (choseFolder == true)
+            {
+                // Start Access with the new database with the Scripts
+                Access.Application oAccess = new Access.Application();
+                oAccess.Visible = true;
+                oAccess.OpenCurrentDatabase(path + @"\" + textBox1.Text + ".accdb", false, "");
+
+                // Run the Start Macro in the Database to execute the scripts
+                oAccess.DoCmd.RunMacro("StartMacro");
+                oAccess.DoCmd.Quit(Access.AcQuitOption.acQuitSaveAll);
+
+                // Release Resources 
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(oAccess);
+                oAccess = null;
+
+
+                // Run the Excel Macros
+                excelMacros(path + @"\QAResultsACPMac.xlsm", "QABeautify");
+                excelMacros(path + @"\CRCMac.xlsm", "QABeautify");
+                excelMacros(path + @"\JCPMac.xlsm", "QABeautify");
+                //  excelMacros(path + @"\ShouderComparisonMac.xlsm", "Shoulderup");
+
+
+                // Delete Macro Files from the folder
+                deleteFile("QAResultsACPMac.xlsm");
+                deleteFile("CRCMac.xlsm");
+                deleteFile("JCPMac.xlsm");
+                //  deleteFile("ShoulderComparisonMac.xlsm");
+                deleteFile("*.bas");
+
+
+                // Let User know that the program is done 
+                DialogResult result = MessageBox.Show("Processing Has Been Completed", "VDOT Processing", MessageBoxButtons.OK);
+                choseFolder = false; 
+            }
+        }
+
+        /*
+         * excelMacros will start and run the excel macros 
+         */
+
+
+        private void excelMacros(string excelMac, string macName)
+        {
+            // Open Excel
+            object oMissing = System.Reflection.Missing.Value;
+            Excel.Application oExcel = new Excel.Application();
+            oExcel.Visible = true;
+            Excel.Workbooks oBooks = oExcel.Workbooks;
+            Excel._Workbook oBook = null;
+            oBook = oBooks.Open(excelMac, oMissing, oMissing, oMissing, oMissing, oMissing, oMissing, oMissing, oMissing, oMissing, oMissing, oMissing, oMissing, oMissing, oMissing);
+
+            // Run the Excel Macro
+            oExcel.Run(macName);
+            oBook.Close(false, oMissing, oMissing);
+            oExcel.Quit();
+
+
+            //Release Resources
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(oBook);
+            oBook = null;
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(oBooks);
+            oBooks = null;
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(oExcel);
+            oExcel = null;
+
+        }
+
+
+        /* RenameOldFiles()
+         * Checks if old processed excel files exist in the folder and appends the time to the front of the files
+         */ 
+
+        private void RenameOldFiles()
+        {
+            if(System.IO.File.Exists(path + @"\ACP_QA_Results.xlsx"))
+            {
+                System.IO.File.Move(path + @"\ACP_QA_Results.xlsx", path + @"\" + DateTime.Now.ToString("HHmmss") + "ACP_QA_Results.xlsx");
+            }
+
+            if (System.IO.File.Exists(path + @"\CRCP_QA_Results.xlsx"))
+            {
+                System.IO.File.Move(path + @"\CRCP_QA_Results.xlsx", path + @"\" + DateTime.Now.ToString("HHmmss") + "CRCP_QA_Results.xlsx");
+            }
+
+            if (System.IO.File.Exists(path + @"\JRCP_QA_Results.xlsx"))
+            {
+                System.IO.File.Move(path + @"\JRCP_QA_Results.xlsx", path + @"\" + DateTime.Now.ToString("HHmmss") + "JRCP_QA_Results.xlsx");
+            }
+
+            if (System.IO.File.Exists(path + @"\TableForCompareACP.xlsx"))
+            {
+                System.IO.File.Move(path + @"\TableForCompareACP.xlsx", path + @"\" + DateTime.Now.ToString("HHmmss") + "TableForCompareACP.xlsx");
+            }
+
+            if (System.IO.File.Exists(path + @"\TableForCompareCRC.xlsx"))
+            {
+                System.IO.File.Move(path + @"\TableForCompareCRC.xlsx", path + @"\" + DateTime.Now.ToString("HHmmss") + "TableForCompareCRC.xlsx");
+            }
+
+            if (System.IO.File.Exists(path + @"\TableForCompareJCP.xlsx"))
+            {
+                System.IO.File.Move(path + @"\TableForCompareJCP.xlsx", path + @"\" + DateTime.Now.ToString("HHmmss") + "TableForCompareJCP.xlsx");
+            }
+
+            if (System.IO.File.Exists(path + @"\ShoulderComparison.xlsx"))
+            {
+                System.IO.File.Move(path + @"\ShoulderComparison.xlsx", path + @"\" + DateTime.Now.ToString("HHmmss") + "ShoulderComparison.xlsx");
+            }
+
+            if (System.IO.File.Exists(path + @"\QESInHouseComparisons.xlsx"))
+            {
+                System.IO.File.Move(path + @"\QESInHouseComparisons.xlsx", path + @"\" + DateTime.Now.ToString("HHmmss") + "QESInHouseComparisons.xlsx");
+            }
+        }
+
+
+        /* deleteFile
+         *  Will delete a file based on a string passed to it if it exists in the folder
+         */ 
+
+        private void deleteFile(string fileName)
+        {
+            // Get the FullPath of the File you want to delete
+            String FullPath = path + @"\" + fileName;
+
+            // If the file exists, delete that file
+            if (System.IO.File.Exists(FullPath))
+            {
+                System.IO.File.Delete(FullPath);
+            }
+        }
+
+
+    }
+}
